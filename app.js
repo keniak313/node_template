b23ef4b6ebeec182b760938dfa9d6d70d255f585
keniak313 <65606607+keniak313@users.noreply.github.com>
2025-03-01 23:42:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 

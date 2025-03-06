b8b042cb7a1de1b8691805633be64c541e364f2b
Pawel <none>
2025-03-06 14:43:05 +0100

    added Prisma and Prisma Session

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { indexRouter } from "./routes/indexRouter.js";
 import { exampleRouter } from "./routes/exampleRouter.js";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,14 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-app.use("/example", exampleRouter)
-app.use("/", indexRouter)
-
-
-
+app.use("/example", exampleRouter);
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Listening on: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Listening on: ${PORT}`);
+});
